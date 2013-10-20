f48d1d76e7d349bf9cb56f1c3a90f5acfe933e66
Nikolas <ngbravo@linuxmint.linuxmint-domain>
2013-10-20 20:38:27 -0300

    Crawler: Se arreglo lo que pidio Martin. Se agrego Rotten Tomatoes.

--- movie-crawler.js
+++ movie-crawler.js
@@ -9,29 +9,55 @@ var fs = require('node-fs'),
     url = require('url'),
     Crawler = require("simplecrawler").Crawler
 
-var myCrawler = new Crawler("www.imdb.com")
-myCrawler.initialPath = "/search/title?title_type=feature,tv_movie,short";
-//myCrawler.interval = 1;
-myCrawler.maxConcurrency = 100;
-myCrawler.timeout = 1500;
-myCrawler.listenerTTL = 1500;
+var myCrawlerIMBD = new Crawler("www.imdb.com")
+myCrawlerIMBD.initialPath = "/search/title?title_type=feature,tv_movie,short";
+//myCrawlerIMBD.interval = 1;
+myCrawlerIMBD.maxConcurrency = 100;
+myCrawlerIMBD.timeout = 1500;
+myCrawlerIMBD.listenerTTL = 1500;
 
 
 
-myCrawler.on("fetchstart",function(queueItem){
+myCrawlerIMBD.on("fetchstart",function(queueItem){
 		console.log("Starting request for:",queueItem.url);
 	})
 
-myCrawler.on("fetchcomplete",function(queueItem, responseBuffer, response) {
+myCrawlerIMBD.on("fetchcomplete",function(queueItem, responseBuffer, response) {
     console.log("I just received %s (%d bytes)",queueItem.url,responseBuffer.length);
     console.log("It was a resource of type %s",response.headers['content-type']);
-    if(queueItem.url.indexOf("title/tt") != -1)
+    if(queueItem.url.indexOf("title/tt") != -1 && queueItem.url.indexOf("criticreviews") == -1 && queueItem.url.indexOf("showtimes") == -1)
     {
-     db.crawlercollection.save({url:queueItem.url, html:responseBuffer});
+     db.crawlercollection.save({url:queueItem.url, html:responseBuffer, sitio:"IMDB"});
+     //console.log(responseBuffer.toString());
+    }
+
+    // Do something with the data in responseBuffer
+});
+//---------------------------------------------------------------------------
+var myCrawlerRT = new Crawler("www.rottentomatoes.com")
+myCrawlerRT.initialPath = "/";
+//myCrawlerRT.interval = 1;
+myCrawlerRT.maxConcurrency = 100;
+myCrawlerRT.timeout = 1500;
+myCrawlerRT.listenerTTL = 1500;
+
+
+
+myCrawlerRT.on("fetchstart",function(queueItem){
+        console.log("Starting request for:",queueItem.url);
+    })
+
+myCrawlerRT.on("fetchcomplete",function(queueItem, responseBuffer, response) {
+    console.log("I just received %s (%d bytes)",queueItem.url,responseBuffer.length);
+    console.log("It was a resource of type %s",response.headers['content-type']);
+    if(queueItem.url.indexOf("/m/") != -1)
+    {
+     db.crawlercollection.save({url:queueItem.url, html:responseBuffer, sitio:"Rotten Tomatoes"});
      //console.log(responseBuffer.toString());
     }
 
     // Do something with the data in responseBuffer
 });
 
-myCrawler.start();
+myCrawlerIMBD.start();
+myCrawlerRT.start();
