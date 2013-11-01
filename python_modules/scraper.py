658ec77b2752961638bc84d078524f3c4fdd0f22
Rodrigo Saffie <rasaffie@uc.cl>
2013-10-31 22:19:02 -0400

    IMDB: Cambio tabs por espacios + limpieza de codigo

--- python_modules/scraper.py
+++ python_modules/scraper.py
@@ -3,9 +3,9 @@ import scraperIMDB
 import threading
 
 def main():
-	t1 = threading.Thread(target=scraperIMDB.mainScraperIMDB)
-	t2 = threading.Thread(target=scraperRT.mainScraperRotten)
-	t1.start(); t2.start()
+    t1 = threading.Thread(target=scraperIMDB.mainScraperIMDB)
+    t2 = threading.Thread(target=scraperRT.mainScraperRotten)
+    t1.start(); t2.start()
 
 if __name__ == '__main__':
-	main()
+    main()
